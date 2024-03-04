cb8f7ff70029ddef8de7a33542dfbc851acee578
PrashantPatil10178 <patilanilprashant178@gmail.com>
2024-03-04 22:56:15 +0530

    postman changes

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -3,6 +3,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 const registerUser = asyncHandler(async function (req, res) {
   return res.status(200).json({
     message: "ok",
+    Backend: "This is a Test",
   });
 });
 
