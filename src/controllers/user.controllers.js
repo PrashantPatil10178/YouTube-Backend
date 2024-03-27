258bef556fbe4332161a8ca97815e48d0b806e55
PrashantPatil10178 <patilanilprashant178@gmail.com>
2024-03-27 22:04:57 +0530

    Added Aggretion Piplines

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -374,11 +374,34 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
         isSubscribed: {
           $cond: {
             if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+            then: true,
+            else: false,
           },
         },
       },
     },
+    {
+      $project: {
+        fullName: 1,
+        username: 1,
+        subscribersCount: 1,
+        channelsSubscribedToCount: 1,
+        isSubscribed: 1,
+        avatar: 1,
+        coverImage: 1,
+        email: 1,
+      },
+    },
   ]);
+  if (!channel?.length) {
+    throw new ApiError(404, "Channel does not exists");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, channel[0], "User Channel Fetched Successfully")
+    );
 });
 
 export {
